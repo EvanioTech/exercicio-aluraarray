617bfb08fee541041a8b069f2047b55ddabfe74a
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2024-01-27 11:56:57 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let listaGenerica = [];
+console.log(listaGenerica);
+
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+console.log(linguagensDeProgramacao);
+console.log(linguagensDeProgramacao.length);
+
+linguagensDeProgramacao.push('java', 'Ruby', 'Golang');
+
+
+
+
+console.log(linguagensDeProgramacao);
+
+let names = ['Jose', 'Maria' , 'Matias'];
+
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
